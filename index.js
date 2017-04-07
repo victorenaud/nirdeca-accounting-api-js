0fd0bc002079012205722043fa3e9cb3a434207a
Acedrin <victor.enaud@hotmail.fr>
2017-04-07 14:36:21 +0200

    NAA-2 : Refonte du lancement du serveur et séparation des routes

--- index.js
+++ index.js
@@ -1,54 +1,29 @@
-const Hapi = require('hapi')
+const Hapi = require('hapi');
+const pjson = require('./package.json');
 
-function createServer(options) {
-    const server = new Hapi.Server()
-    const pjson = require('./package.json')
-    const users = require('./data/users.json')
-    const database  = require('./database.js');
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 3000
+});
 
-    server.connection(options)
-    server.route({
-        method: 'GET',
-        path: '/',
-        config: {
-            description: 'Give basics description of the API',
-            tags: ['api']
-        },
-        handler(request, reply) {
-            reply({
-                description: pjson.description,
-                version: pjson.version
-            })
-        }
-    })
+server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+        description: 'Give basics description of the API',
+        tags: ['api']
+    },
+    handler(request, reply) {
+        reply({
+            description: pjson.description,
+            version: pjson.version
+        })
+    }
+});
 
-    server.route({
-        method: 'GET',
-        path: '/users',
-        config: {
-            description: 'Give the list of all users',
-            tags: ['api', 'users']
-        },
-        handler(request, reply) {
-            var knew_users = database.select().table('users');
-            reply(knew_users)
-        }
-    })
+server.route(require('./users/usersRoute.js'));
 
-    server.route({
-        method: 'GET',
-        path: '/users/{id}',
-        config: {
-            description: 'Give the the details of an user',
-            tags: ['api', 'users']
-        },
-        handler(request, reply) {
-            id = request.params.id
-            reply(users.users[id - 1])
-        }
-    })
-
-    return server
-}
-
-exports.createServer = createServer
+server.start((err) => {
+    console.log('Server running at:', server.info.uri);
+});
