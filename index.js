1272f76809437f9256b80445edcf1bf09f6fef05
Acedrin <victor.enaud@hotmail.fr>
2017-04-10 23:08:08 +0200

    bsr(NAA-2) : Bit of reordering of files

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 const Hapi = require('hapi');
 const pjson = require('./package.json');
-
 const server = new Hapi.Server();
-server.connection({
-    host: 'localhost',
-    port: 3000
-});
 
-server.route({
-    method: 'GET',
-    path: '/',
-    config: {
-        description: 'Give basics description of the API',
-        tags: ['api']
-    },
-    handler(request, reply) {
-        reply({
-            description: pjson.description,
-            version: pjson.version
-        })
-    }
-});
 
-server.route(require('./users/usersRoute.js'));
+function createServer(options) {
+    server.connection(options);
+    server.route({
+        method: 'GET',
+        path: '/',
+        config: {
+            description: 'Give basics description of the API',
+            tags: ['api']
+        },
+        handler(request, reply) {
+            reply({
+                description: pjson.description,
+                version: pjson.version
+            })
+        }
+    });
 
-server.start((err) => {
-    console.log('Server running at:', server.info.uri);
-});
+    server.route(require('./app/users/usersRoute.js'));
+
+    return server;
+}
+
+exports.createServer = createServer;
