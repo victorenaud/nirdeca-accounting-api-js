b7ff9195eea422f50d056fb436a205cb20b29bc1
Acedrin <victor.enaud@hotmail.fr>
2017-06-29 23:02:50 +0200

    feat(NAA-3): Saving date in the right format

--- server/use_cases/user/addUser.js
+++ server/use_cases/user/addUser.js
@@ -2,14 +2,14 @@ const userDomain = require('../../domain/index').userDomain;
 const userRepository = require('../../infrastructure/persistence/index').userRepository;
 
 function addUser(name, lastName, email) {
-    const now = new Date();
-    
+    const now = new Date().toISOString();
+
     const user = {
         name: name,
         last_name: lastName,
         email: email,
-        updated_at: now.getDate(),
-        created_at: now.getDate()
+        updated_at: now,
+        created_at: now
     };
 
     return userDomain.addUser(userRepository, user);
